afb788090ee1f6a5ca72b6a05d9f1ee2a99d3beb
Saugat <saugat.adhikari@docsumo.com>
2019-11-08 13:43:28 +0545

    Written function for uploading multiple files, and added user_doc_id as an optional parameter in upload_file method

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,8 @@ from docsumo import Docsumo
 from loguru import logger
 # r = Docsumo(apikey="Trt7Rukb9BZ5rFsO1s7zlGFlVPJRDZCJr0cByjfiK8jqjSLyQjpgd4ze2jOI")
 
-r = Docsumo(apikey="enmYrHxKhAu3EttfofbE6xnYOKRRL18W9OrfRucXQXNJowRn6bmL9jzTFTJ8",url='https://apptesting.docsumo.com')
-# r = Docsumo()
+# r = Docsumo(apikey="",url='https://apptesting.docsumo.com')
+r = Docsumo()
 
 logger.info(r.limit())
 logger.info(r.documents_list())
