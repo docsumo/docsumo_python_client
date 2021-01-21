056ebd4604c8ef270a64f1c077e89aef9dcd51ea
janardan.banjara <janardan.banjara@docsumo.com>
2021-01-21 13:44:19 +0545

    document summary method updated, method added to upload file via url and get detail of single doc

--- docsumo/Docsumo.py
+++ docsumo/Docsumo.py
@@ -14,12 +14,12 @@ class Docsumo:
     Args:
         apiKey:``str``
             API key provided to user.
-        url:``str``    
+        url:``str``
             Url of docsumo api
         version:``str``
             API version.
     Returns:
-        Docsumo class object.            
+        Docsumo class object.
     """
 
     def __init__(self, apikey=None, url=None, version="v1"):
@@ -101,18 +101,18 @@ class Docsumo:
             limit:``int``
                 Number of documents whose details are to be fetched.
             status:``list``
-                The status of the documents ``processed`` ``new`` ``review_required`` 
+                The status of the documents ``processed`` ``new`` ``review_required``
                 ``review_skipped``.
             created_date_greater_than: ``str``
                 format ``YYYY-MM-DD``
             created_date_less_than: ``str``
-                format ``YYYY-MM-DD`` 
+                format ``YYYY-MM-DD``
 
         Returns:
             Document list with details : ``dict``
 
             .. code-block:: json
-            
+
                 {
                     'data': {
                         'documents': [{
@@ -191,10 +191,10 @@ class Docsumo:
 
         Args:
             doc_id:``str``
-                Valid Document Id of the document whose detail is required. 
+                Valid Document Id of the document whose detail is required.
 
         Returns:
-            Document details : ``dict`` 
+            Document details : ``dict``
 
             .. code-block:: json
 
@@ -256,7 +256,7 @@ class Docsumo:
                 }
         """
 
-        url = "{}/api/{}/eevee/apikey/documents/summary/".format(self.url, self.version)
+        url = "{}/api/{}/mew/apikey/documents/summary/".format(self.url, self.version)
         response = requests.request("GET", url, headers=self.headers)
         original_response = response.json()
         return original_response
@@ -272,10 +272,10 @@ class Docsumo:
             user_doc_id: ``str``
                 document id given by user
         Returns:
-            Document upload details for successful uploads : ``dict``                          
-        
+            Document upload details for successful uploads : ``dict``
+
             .. code-block:: json
-            
+
                 {
                     'data': {
                             'created_at': 'Mon, 22 Apr 2019 11:56:53 GMT',
@@ -327,7 +327,7 @@ class Docsumo:
             "uploaded_from": (None, "api"),
         }
         if user_doc_id:
-            multipart_form_data["user_doc_id"]: (None, user_doc_id)
+            multipart_form_data["user_doc_id"] = (None, user_doc_id)
 
         response = requests.post(url, files=multipart_form_data, headers=headers)
         original_response = response.json()
@@ -338,11 +338,11 @@ class Docsumo:
         delete document
         Args:
             doc_ids:``list``
-                list of doc_ids 
-        Returns: 
+                list of doc_ids
+        Returns:
             Doc_ids Detail: `json`
                 .. code-block:: json
-            
+
                     {
                         'deleted_doc': [],
                         'not_deleted_doc': [{'doc_id': 'ghsd',
@@ -359,8 +359,8 @@ class Docsumo:
                     response = requests.request(
                         "POST", url, headers={"apikey": self.apikey}
                     )
-                    _ = response.json()
-                return {"deleted_doc": doc_ids, "not_deleted_doc": []}
+                    response = response.json()
+                return response
             else:
                 raise ValueError("doc_ids should have have atleast one doc_id")
         else:
@@ -389,9 +389,9 @@ class Docsumo:
         Returns ocr detail for document
         Args:
             doc_id:``str``
-                Valid Document Id of the document whose detail is required. 
+                Valid Document Id of the document whose detail is required.
         Returns:
-            Document ocr details : ``dict`` 
+            Document ocr details : ``dict``
         """
 
         url = "{}/api/{}/eevee/apikey/ocr/{}/".format(self.url, self.version, doc_id)
@@ -404,7 +404,7 @@ class Docsumo:
         update value and position of item
         Args:
             doc_id:``str``
-                Valid Document Id of the document whose detail is required. 
+                Valid Document Id of the document whose detail is required.
             item_id: ``int ``
                 Item id to update
             value: ``str``
@@ -412,7 +412,7 @@ class Docsumo:
             position: ``list``
                 list of position as ``[x, y, x1, y1]
         Returns:
-            responses : ``dict`` 
+            responses : ``dict``
         """
         data = {"value": value, "position": position}
         url = "{}/api/{}/eevee/apikey/update/item/{}/{}/".format(
@@ -427,38 +427,38 @@ class Docsumo:
         add new item to list
         Args:
             doc_id:``str``
-                Valid Document Id of the document whose detail is required. 
+                Valid Document Id of the document whose detail is required.
             item_dict: ``dict``
                 complete dict of item
                     ..code-block :: json
                         {
                             "content": {
-                            "is_valid_format": True, 
-                            "orig_value": "test", 
-                            "page": 1, 
-                            "position": [], 
-                            "validation_source": "human", 
+                            "is_valid_format": True,
+                            "orig_value": "test",
+                            "page": 1,
+                            "position": [],
+                            "validation_source": "human",
                             "value": "test"
-                            }, 
-                            "doc_id": "8cb8ad65d6684165b32eed755a416138", 
-                            "filters": {}, 
-                            "format": "%s", 
-                            "format_message": "value should be string", 
-                            "id": 1001005, 
-                            "ignore": False, 
-                            "label": "new label", 
-                            "low_confidence": False, 
-                            "no_items_row": 1, 
-                            "p_title": "Basic Information", 
-                            "p_type": "basic_info", 
-                            "parent_id": 1001, 
-                            "time_spent": 4, 
-                            "type": "string", 
-                            "user_id": "5d4122f07c38c76c74073af7", 
+                            },
+                            "doc_id": "8cb8ad65d6684165b32eed755a416138",
+                            "filters": {},
+                            "format": "%s",
+                            "format_message": "value should be string",
+                            "id": 1001005,
+                            "ignore": False,
+                            "label": "new label",
+                            "low_confidence": False,
+                            "no_items_row": 1,
+                            "p_title": "Basic Information",
+                            "p_type": "basic_info",
+                            "parent_id": 1001,
+                            "time_spent": 4,
+                            "type": "string",
+                            "user_id": "5d4122f07c38c76c74073af7",
                             "validation_source": "human"
                         }
         Returns:
-            Responses : ``dict`` 
+            Responses : ``dict``
         """
 
         data = item_dict
@@ -477,14 +477,14 @@ class Docsumo:
             file_paths:``list``
                 List of document paths to be uploaded.
             doc_type:``str``
-                Document type. Currently supported: (Invoice, Invoice_drip, bank_statements)`` 
+                Document type. Currently supported: (Invoice, Invoice_drip, bank_statements)``
             user_doc_ids: ``list``
                 List of Document Id to be uploaded. Optional
         Returns:
-            Document upload details for successful uploads : ``dict``                          
-        
+            Document upload details for successful uploads : ``dict``
+
             .. code-block:: json
-            
+
                 {
                     'files_uploaded': [{
                                             'data': {
@@ -516,7 +516,7 @@ class Docsumo:
                                             },
                                             {
                                             'metadata': {
-                                                'user_doc_id': '333', 
+                                                'user_doc_id': '333',
                                                 'title': 'invoice_2.jpg'
                                                 },
                                                 'error': 'Duplicated user_id ',
@@ -593,3 +593,145 @@ class Docsumo:
 
     def __repr__(self):
         return "Docsumo API"
+
+    def upload_file_custom(
+        self, file_url, file_name, file_type, doc_title, user_doc_id=None
+    ):
+        """
+        Uploads valid documents for processing.
+        Args:
+            file_url:``str``
+                Link to file or base 64 encoding
+            file_name:``str``
+                Title of the file
+            file_type:``str``
+                base64 if file is base64 encoded else url
+            doc_title:``str``
+                Document title. You can get title using ``user_detail_credit_limit``.
+            user_doc_id: ``str``
+                Document id given by user
+        Returns:
+            Document upload details for successful uploads : ``dict``
+
+            .. code-block:: json
+
+                {
+                    'data': {
+                            'created_at': 'Mon, 22 Apr 2019 11:56:53 GMT',
+                            'doc_id': '16474639f3da47beb87788c875503009',
+                            'email': 'bkrm.dahal@gmail.com',
+                            'status': 'new',
+                            'title': 'c05440f11dda4cfba70a856114e12a50.jpg',
+                            'type': 'invoice',
+                            'url_original': 'https://test.png',
+                            'user_id': '5cbcb201a79f0f1d983a34ea'
+                    },
+                    'error': '',
+                    'error_code': '',
+                    'message': '',
+                    'status': 'success',
+                    'status_code': 200
+                }
+        """
+        doc_type = doc_title
+
+        if not self.doc_titles:
+            user_detail = self.user_detail_credit_limit()["data"]
+            doc_titles = user_detail.get("document_types", None)
+
+            if not doc_titles:
+                return {
+                    "error": "no key document_types in method user_detail_credit_limit"
+                }
+            else:
+                self.doc_titles = {i["title"]: i["value"] for i in doc_titles}
+
+        if not doc_type in self.doc_titles:
+            raise UnsupportedDocumentType(
+                "{} document type is not supported. Supported types: {}".format(
+                    doc_type, list(self.doc_titles.values())
+                )
+            )
+        else:
+            doc_type = self.doc_titles[doc_type]
+
+        url = "{}/api/{}/eevee/apikey/upload/custom/".format(self.url, self.version)
+        headers = {"apikey": self.apikey}
+
+        # filename = os.path.basename(file_path)
+
+        multipart_form_data = {
+            "file": (None, file_url),
+            "filename": (None, file_name),
+            "file_type": (None, file_type),
+            "type": (None, doc_type),
+            "uploaded_from": (None, "api"),
+        }
+        if user_doc_id:
+            multipart_form_data["user_doc_id"]: (None, user_doc_id)
+
+        response = requests.post(url, files=multipart_form_data, headers=headers)
+        original_response = response.json()
+        return original_response
+
+    def doc_detail_single(self, doc_id):
+        """
+        Uploads valid documents for processing.
+        Args:
+            doc_id:``str``
+                Unique document id of a document
+        Returns:
+            Details of document specified by doc_id : ``dict``
+
+            .. code-block:: json
+
+                {
+                    "data": {
+                        "document": {
+                            "data": {
+                                "col1": "",
+                                "col2": "",
+                                "col3": ""
+                            },
+                            "doc_id": "5eb50cb4b6814bbd83fd4eb3c43978bf",
+                            "pages": [
+                                {
+                                    "id": 1,
+                                    "image": {
+                                        "height": 2339,
+                                        "url": "https://testing-docsumo-documents.s3.amazonaws.com/5fcf257f72ccee82d405e881/97082/5eb50cb4b6814bbd83fd4eb3c43978bf_image.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAI2SXTX7KMYAKIFCA%2F20210120%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20210120T055014Z&X-Amz-Expires=43200&X-Amz-SignedHeaders=host&X-Amz-Signature=f4b6ee310ebdd8380d776c124df79f6ce4a9a772c423545bbf175c6d2a3c69d7",
+                                        "width": 1653
+                                    }
+                                }
+                            ],
+                            "preview_image": {
+                                "height": 1169,
+                                "url": "https://testing-docsumo-documents.s3.ap-south-1.amazonaws.com/5fcf257f72ccee82d405e881/97082/5eb50cb4b6814bbd83fd4eb3c43978bf_thumbnail.jpg",
+                                "width": 826
+                            },
+                            "type": "invoice",
+                            "type_title": "Invoice",
+                            "uploaded_by": {
+                                "avatar_url": "",
+                                "email": "bs_check@apptesting.com",
+                                "full_name": "Janardan Banjara",
+                                "user_id": "5fcf257f72ccee82d405e881"
+                            },
+                            "user_id": "5fcf257f72ccee82d405e881"
+                        }
+                    },
+                    "error": "",
+                    "error_code": "",
+                    "message": "",
+                    "status": "success",
+                    "status_code": 200
+                }
+        """
+        url = "{}/api/{}/eevee/apikey/documents/detail/{}/".format(
+            self.url, self.version, doc_id
+        )
+        headers = {"apikey": self.apikey}
+
+        response = requests.get(url, headers=headers)
+        original_response = response.json()
+        return original_response
